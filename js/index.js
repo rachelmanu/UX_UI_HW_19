66c384c1a2bc437d10a9a6b66f9ed1aa90b25c5d
rachelmanu <142449332+rachelmanu@users.noreply.github.com>
2024-01-31 18:08:13 -0500

    1/31updates
    includes js animation

---
+++ js/index.js
@@ -0,0 +1,10 @@
+//image hover animation
+$( ".skillImages img" ).hover(
+  function() {
+    $( this ).addClass( "hover" );
+  }, function() {
+    $( this ).removeClass( "hover" );
+  }
+);
+
+//text scroll animation
